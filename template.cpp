98fe7b94e29f43a9a9a5692072c2c092bbe765f6
1ASPER <asperthex@gmail.com>
2024-12-26 20:38:24 +0600

    template code

---
+++ template.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define all(obj) obj.begin(), obj.end()
+#define sz(obj) (int)obj.size()
+#define pii pair<int, int>
+#define pb push_back
+
+using ll = long long;
+using namespace std;
+
+template <typename T = int>
+istream& operator>>(istream &in, vector<T> &v) {
+    for (auto &x : v) in >> x;
+    return in;
+}
+
+template <typename T = int>
+ostream& operator<<(ostream &out, const vector<T> &v) {
+    for (const T &x : v) out << x << ' ';
+    return out;
+}
+ 
+ 
+void solve() {
+    
+}
+ 
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int T = 1;
+    //cin >> T;
+ 
+    while (T--) {
+        solve();
+    }
+}
